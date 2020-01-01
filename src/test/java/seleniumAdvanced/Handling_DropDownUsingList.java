537f8665a1589058783dad64033df88203060493
jagatheshwaran <jagatheshwaran.n29@gmail.com>
2020-01-01 16:08:04 +0530

    Dropdown selection using list

--- src/test/java/seleniumAdvanced/Handling_DropDownUsingList.java
+++ src/test/java/seleniumAdvanced/Handling_DropDownUsingList.java
@@ -4,12 +4,9 @@
  */
 package seleniumAdvanced;
 
-import java.io.IOException;
-
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,6 +14,45 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Handling_DropDownUsingList {
 
 	static WebDriver driver;
+
+	public static void selectDropValue() throws InterruptedException {
+
+		// Set the Browser Driver path
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/BrowserDrivers/chromedriver.exe");
+
+		// Driver instance is created
+		driver = new ChromeDriver();
+
+		// manage().window().maximize() - Maximize Browser Window
+		driver.manage().window().maximize();
+
+		// get() - It will take you to below html web page
+		driver.get("D:\\ECLIPSE_ENV\\Workspace\\SeleniumBasics\\src\\test\\resources\\TestFiles\\Dropdown.html");
+
+		WebElement Dropdown = driver.findElement(By.xpath("//div[@class='select-selected']"));
+		Dropdown.click();
+
+		// Taking list of Options from DropDown and store it in List WebElement, Iterate
+		// through options to select the value passed in code
+		List<WebElement> options = driver.findElements(By.xpath("//ul[@class='select-items']/li"));
+		System.out.println("Options : " + options.size());
+		boolean flag = false;
+		for (WebElement option : options) {
+			if (option.getText().equalsIgnoreCase("Google Chrome")) {
+				flag = true;
+				option.click();
+			}
+		}
+		if (flag == false) {
+			System.out.println(flag + " Given element not found");
+		}
+
+		Thread.sleep(2000);
+
+		// close() - It will close Browser window
+		driver.close();
+
+	}
 
 	public static void select2ndLastElement() throws InterruptedException {
 
@@ -32,64 +68,28 @@ public class Handling_DropDownUsingList {
 		// get() - It will take you to below html web page
 		driver.get("D:\\ECLIPSE_ENV\\Workspace\\SeleniumBasics\\src\\test\\resources\\TestFiles\\Dropdown.html");
 
-		WebElement Dropdown = driver.findElement(By.xpath("//input[@name='browser']"));
+		WebElement Dropdown = driver.findElement(By.xpath("//div[@class='select-selected']"));
 		Dropdown.click();
 
-		// Taking list of Options from DropDown
-		List<WebElement> options = driver.findElements(By.xpath("//datalist[@id='browsers']//option"));
-		System.out.println("DropDown option list size : " + options.size());
+		// Taking list of Options from DropDown and store it in List WebElement
+		List<WebElement> options = driver.findElements(By.xpath("//ul[@class='select-items']/li"));
+		System.out.println("Options : " + options.size());
+
 		Thread.sleep(2000);
 
-		// It will select 2nd last option value in DropDown
+		// The below code will select 2nd last option value from DropDown
 		WebElement option = options.get(options.size() - 2);
 		option.click();
-		// Dropdown.click();
+
 		Thread.sleep(2000);
+
+		// close() - It will close Browser window
 		driver.close();
 
 	}
 
-	public static void main(String ar[]) throws IOException, InterruptedException {
-
-		// Set the Browser Driver path
-		System.setProperty("webdriver.chrome.driver", "src/main/resources/BrowserDrivers/chromedriver.exe");
-
-		// Driver instance is created
-		driver = new ChromeDriver();
-
-		// manage().window().maximize() - Maximize Browser Window
-		driver.manage().window().maximize();
-
-		// get() - It will take you to below html web page
-		driver.get("D:\\ECLIPSE_ENV\\Workspace\\SeleniumBasics\\src\\test\\resources\\TestFiles\\Dropdown.html");
-
-		WebElement Dropdown = driver.findElement(By.xpath("//input[@name='browser']"));
-		Dropdown.click();
-
-		Thread.sleep(2000);
-
-		// Taking list of Options from DropDown and selecting Option what we passed in
-		// code
-		List<WebElement> options = driver.findElements(By.xpath("//datalist[@id='browsers']/option"));
-		System.out.println("Options : " + options.size());
-		boolean flag = false;
-		for (WebElement option : options) {
-			System.out.println(option.getAttribute("value"));
-			if (option.getAttribute("value").equalsIgnoreCase("Chrome")) {
-				flag = true;
-				JavascriptExecutor js = (JavascriptExecutor) driver;
-				js.executeScript("arguments[0].click", option);
-				Thread.sleep(5000);
-				// Dropdown.click();
-			}
-		}
-		if (flag == false) {
-			System.out.println(flag + " Given element not found");
-		}
-		Thread.sleep(2000);
-
-		// It will close Browser window
-		driver.close();
-
+	public static void main(String ar[]) throws InterruptedException {
+		selectDropValue();
+		select2ndLastElement();
 	}
 }
